3aa8b5ab2527ec47f24eed6a7816bbd3162206b9
Srinivas <srinivassuryapet@gmail.com>
2021-03-08 05:34:17 -0800

    MakeFile

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -2,12 +2,11 @@
 yum install nginx -y
 systemctl enable nginx
 systemctl start nginx
-curl -s -L -o /tmp/frontend.zip "https://dev.azure.com/DevOps-Batches/e869c1fe-489d-4f58-b676-fb75fe4e9e12/_apis/git/repositories/18fbb315-ed99-4a65-9b79-e7eec2f3e1b1/items?path=%2F&versionDescriptor%5BversionOptions%5D=0&versionDescriptor%5BversionType%5D=0&versionDescriptor%5Bversion%5D=master&resolveLfs=true&%24format=zip&api-version=5.0&download=true"
+curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip"
 cd /usr/share/nginx/html || exit
-rm -rf ./*
+rm -rf *
 unzip /tmp/frontend.zip
-cd frontend-main/ || exit
-mv static/* /usr/share/nginx/html
+mv static/* .
 rm -rf static README.md
 mv localhost.conf /etc/nginx/default.d/roboshop.conf
 systemctl restart nginx
