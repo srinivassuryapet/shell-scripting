9645edc6397d90248fe93132c02cbadcc54f86d6
Srinivas <srinivassuryapet@gmail.com>
2021-03-09 05:34:42 -0800

    check root user and check variable is empty

--- roboshop/components/08-if.sh
+++ roboshop/components/08-if.sh
@@ -2,9 +2,11 @@
 if [ "$USER" != "root" ] ; then
   echo "Hey, you are not a root user"
 fi
+
 read -p "Enter your name =" name
+
 if [ -z "$name" ] ; then
-  echo "Your have not entered any name"
+  echo "You have not entered any name"
 else
   echo "$name, welcome"
 fi
