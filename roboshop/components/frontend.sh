4198d27a9c55607b7af45cb350bf6711ded67dea
Srinivas <srinivassuryapet@gmail.com>
2021-03-12 21:41:44 -0800

    check root user before executing script

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 COMPONENT=frontend
-source components/common.sh
+source /home/centos/shell-scripting/roboshop/components/common.sh
 Print"Installing Nginx"
 #yum install nginx -y
 Print"Starting Nginx Service"
