f4a276333724e765c72a67a6745ffa3b26f84c54
Srinivas <srinivassuryapet@gmail.com>
2021-03-17 00:31:52 -0700

    check root user before executing script

--- roboshop/components/common.sh
+++ roboshop/components/common.sh
@@ -5,6 +5,14 @@ if [ "$USER_ID" -ne 0 ]; then
   echo "you must be a root user to execute this script"
   exit 1
 fi
+#Linux based output
+#Print() {
+#  echo -e "$(date +%c) $(hostname) ${COMPONENT} :: $1",
+#}
+#Maven based output
+
 Print() {
-  echo -e "$(date +%c) $(hostname) ${COMPONENT} :: $1",
+  echo -e "[INFO]-------------------< $1 >------------------------"
+  echo -e "[INFO]$2"
+  echo -e "[INFO]-------------------------------------------------"
 }
