04c7fb4816d473a96aa02bb289641a2ae9ad4de5
Srinivas <srinivassuryapet@gmail.com>
2021-03-20 21:30:25 -0700

    check root user before executing script

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -6,14 +6,13 @@ yum install nginx -y
 Stat $?
 Print "Starting Nginx Service"
 exit
-systemctl enable nginx
 systemctl start nginx
 curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip"
 cd /usr/share/nginx/html || exit
 rm -rf *
-unzip /tmp/frontend.zip
-cd frontend-main/ || exit
-mv static/* /usr/share/nginx/html
-rm -rf static README.md
-mv localhost.conf /etc/nginx/default.d/roboshop.conf
+Stat $?
+unzip /tmp/frontend.zip && cd frontend-main/ || exit && mv static/* /usr/share/nginx/html && rm -rf static README.md && mv localhost.conf /etc/nginx/default.d/roboshop.conf
+Stat $?
+systemctl enable nginx
 systemctl restart nginx
+Stat $?
