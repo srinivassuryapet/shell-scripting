53dd8b02630fee575359f36aee4f305b75a1108a
Srinivas <srinivassuryapet@gmail.com>
2021-03-23 09:32:05 -0700

    check root user before executing script

--- roboshop/components/create-instance.sh
+++ roboshop/components/create-instance.sh
@@ -5,5 +5,13 @@ if [ -z "${component}" ]; then
   echo "Provide Input of component name"
   exit 1
 fi
+STATE=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=${component}" --query Reservations[*].Instances[*].[State.Name] --output text)
 
-aws ec2 run-instances --launch-template LaunchTemplateId=lt-00737ccd643dcd705 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${component}}]"
+if [ STATE != "running" ]; then
+  aws ec2 run-instances --launch-template LaunchTemplateId=lt-00737ccd643dcd705 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${component}}]"
+fi
+IPADDRESS=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=${component}" --query 'Reservations[*].Instances[*].PrivateIpAddress' --output text)
+export component
+export IPADDRESS
+envsubt <record.json >/tmp/${component}.json
+#aws route53 change-resource-record-sets --hosted-zone-id Z06632521MY6X8844QA0Y --change-batch file:///tmp/${component}.json
