b6fe807d275197944b154221402c9ad95f35eae9
Srinivas <srinivassuryapet@gmail.com>
2021-03-25 00:40:55 -0700

    check root user before executing script

--- roboshop/components/create-instance.sh
+++ roboshop/components/create-instance.sh
@@ -7,8 +7,9 @@ if [ -z "${component}" ]; then
 fi
 STATE=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=${component}" --query Reservations[*].Instances[*].[State.Name] --output text)
 
-if [ $STATE != "running" ]; then
+if [ "$STATE" != "running" ]; then
   aws ec2 run-instances --launch-template LaunchTemplateId=lt-00737ccd643dcd705 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${component}}]"
+  sleep 30
 fi
 IPADDRESS=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=${component}" --query 'Reservations[*].Instances[*].PrivateIpAddress' --output text)
 export component
