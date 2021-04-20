88d8f0dab24ee4c3e2e83430f058fd976d2a1cff
Srinivas <srinivassuryapet@gmail.com>
2021-04-20 13:24:31 -0700

    check root user before executing script

--- roboshop/components/mysql.sh
+++ roboshop/components/mysql.sh
@@ -2,6 +2,7 @@
 COMPONENT=mysql
 source components/common.sh
 
+Print "setup mysql repo"
 echo '[mysql57-community]
 name=MySQL 5.7 Community Server
 baseurl=http://repo.mysql.com/yum/mysql-5.7-community/el/7/$basearch/
@@ -9,22 +10,37 @@ enabled=1
 gpgcheck=0' > /etc/yum.repos.d/mysql.repo
 Stat $?
 
+Print "Install mysql"
 yum remove mariadb-libs -y && yum install mysql-community-server -y
 Stat $?
 
+Print "start mysql"
 systemctl enable mysqld
 systemctl start mysqld
+Stat $?
+sleep 30
 
-grep temp /var/log/mysqld.log
+#mysql_secure_installation
 
-mysql_secure_installation
+echo "show databases;" | mysql -uroot -ppassword &>/dev/null
+if [ $? -ne 0 ]; then
+  Print "Grab default mysql password"
+  DEFAULT_PASSWORD=$(grep 'temporary password' /var/log/mysqld.log | awk '{print $NF}')
+  Stat $?
 
-mysql -u root -p
+  Print "Resetting mysql default password"
+  mysql --connect-expired-password -uroot -p"${DEFAULT_PASSWORD}" <<EOF
+  ALTER USER 'root'@'localhost' IDENTIFIED BY 'Default_RoboShop*999';
+  uninstall plugin validate_password;
+  ALTER USER 'root'@'localhost' IDENTIFIED BY 'password';
+EOF
+  Stat $?
+fi
 
-> uninstall plugin validate_password;
-> ALTER USER 'root'@'localhost' IDENTIFIED BY 'password';
-
+Print "Downloading Schema"
 curl -s -L -o /tmp/mysql.zip "https://github.com/roboshop-devops-project/mysql/archive/main.zip"
+Stat $?
 
-cd /tmp && unzip mysql.zip && cd mysql-main && mysql -u root -ppassword <shipping.sql
+Print "Load Schema"
+cd /tmp && unzip -o mysql.zip && cd mysql-main && mysql -u root -ppassword <shipping.sql
 
