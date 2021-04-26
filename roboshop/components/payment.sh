beaf3289195ba490fd8070e95b487c987789e0c8
Srinivas <srinivassuryapet@gmail.com>
2021-04-26 01:59:54 -0700

    check root user before executing script

--- roboshop/components/payment.sh
+++ roboshop/components/payment.sh
@@ -10,20 +10,27 @@ Print "Adding Roboshop user"
 id roboshop || useradd roboshop
 Stat $?
 
-Print "Downloading the config"
-rm -rf /home/roboshop/payment && cd /home/roboshop && curl -L -s -o /tmp/payment.zip "https://github.com/roboshop-devops-project/payment/archive/main.zip"
-Stat $?
-unzip -o /tmp/payment.zip && mv payment-main payment
+Print "Download payment code"
+rm -rf /home/roboshop/payment && curl -L -s -o /tmp/payment.zip "https://github.com/roboshop-devops-project/payment/archive/main.zip"
 Stat $?
 
-cd /home/roboshop/payment || exit
+Print "Unzip"
+cd /home/roboshop && unzip -o /tmp/payment.zip && mv payment-main payment
 Stat $?
 
-pip3 install -r requirements.txt
+Print "Installation"
+cd /home/roboshop/payment && pip3 install -r requirements.txt
 Stat $?
-exit
 
-Print "Starting the service"
+Print "Update user details in payment script"
+USER_ID=$(id -u roboshop)
+GROUP_ID=$(id -g roboshop)
+sed -i -e "/^uid/ c uid=${USER_ID}" -e "/^gid/ c gid=${GROUP_ID}" /home/roboshop/payment/payment.ini
+
+chown roboshop:roboshop /home/roboshop -R
+
+Print "Update systemd file and start payment service"
+sed -i -e "s/CARTHOST/cart-ss.srinivassuryapet.ml/" -e "s/USERHOST/user-ss.srinivassuryapet.ml" -e "s/AMQPHOST/rabbitmq-ss.srinivassuryapet.ml" /home/roboshop/payment/systemd.service
 mv /home/roboshop/payment/systemd.service /etc/systemd/system/payment.service
 systemctl daemon-reload && systemctl enable payment && systemctl start payment
 Stat $?
